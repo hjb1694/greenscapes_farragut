e35a0021619a9771146409a5fa17caee5cf67108
Hayden Bradfield <me@haydenbradfield.com>
2026-05-08 22:03:36 -0400

    wip

--- router/router.php
+++ router/router.php
@@ -16,6 +16,12 @@ function use_router($app) {
         return $view->render($response, 'home.page.twig');
     });
 
+    $app->get('/about-us', function (Request $request, Response $response) {
+        $view = Twig::fromRequest($request);
+
+        return $view->render($response, 'about.page.twig');
+    });
+
 
 
 }
